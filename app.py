b4d23f069d8d3c14f77f196132f242db589ffd8e
mdurazob <mdurazob@gmail.com>
2020-04-25 15:51:23 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from shapely.geometry import Point, shape
+
+from flask import Flask
+from flask import render_template
+import json
+
+
+data_path = './input/'
+n_samples = 11
+
+
+def get_location(longitude, latitude, provinces_json):
+    
+    point = Point(longitude, latitude)
+
+    for record in provinces_json['features']:
+        polygon = shape(record['geometry'])
+        if polygon.contains(point):
+            return record['properties']['name']
+    return 'other'
+
+
+with open(data_path + '/geojson/entidadesmexico.json') as data_file:    
+    provinces_json = json.load(data_file)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/data")
+def get_data():
+   ## gen_age_tr = pd.read_csv(data_path + 'gender_age_train.csv')
+    df = pd.read_csv(data_path + 'events.csv')
+   ## ph_br_dev_model = pd.read_csv(data_path + 'phone_brand_device_model.csv')
+
+   ## df = gen_age_tr.merge(ev, how='left', on='device_id')
+  ##  df = df.merge(ph_br_dev_model, how='left', on='device_id')
+    #Get n_samples records
+    df = df[df['longitude'] != 0].sample(n=n_samples)
+
+
+ 
+
+    df['location'] = df.apply(lambda row: get_location(row['longitude'], row['latitude'], provinces_json), axis=1)
+
+    cols_to_keep = ['timestamp', 'longitude', 'latitude', 'tipo', 'economia', 'municipio', 'atencion', 'location', 'mensaje']
+    df_clean = df[cols_to_keep].dropna()
+
+    return df_clean.to_json(orient='records')
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1',port=8080,debug=True)
---
+++ static/js/graphs.js
@@ -0,0 +1,186 @@
+queue()
+    .defer(d3.json, "/data")
+    .await(makeGraphs);
+
+function makeGraphs(error, recordsJson) {
+	
+	//Clean data
+	var records = recordsJson;
+	var dateFormat = d3.time.format("%Y-%m-%d %H:%M:%S");
+	
+	records.forEach(function(d) {
+		d["timestamp"] = dateFormat.parse(d["timestamp"]);
+		d["timestamp"].setMinutes(0);
+		d["timestamp"].setSeconds(0);
+		d["longitude"] = +d["longitude"];
+		d["latitude"] = +d["latitude"];
+	});
+
+	//Create a Crossfilter instance
+	var ndx = crossfilter(records);
+
+	//Define Dimensions
+	var dateDim = ndx.dimension(function(d) { return d["timestamp"]; });
+	var economiaDim = ndx.dimension(function(d) { return d["economia"]; });
+	var tipoDim = ndx.dimension(function(d) { return d["tipo"]; });
+	var atencionDim = ndx.dimension(function(d) { return d["atencion"]; });
+	var locationdDim = ndx.dimension(function(d) { return d["location"]; });
+	var allDim = ndx.dimension(function(d) {return d;});
+
+
+	//Group Data
+	var numRecordsByDate = dateDim.group();
+	var economiaGroup = economiaDim.group();
+	var tipoGroup = tipoDim.group();
+	var atencionGroup = atencionDim.group();
+	var locationGroup = locationdDim.group();
+	var all = ndx.groupAll();
+
+
+	//Define values (to be used in charts)
+	var minDate = dateDim.bottom(1)[0]["timestamp"];
+	var maxDate = dateDim.top(1)[0]["timestamp"];
+
+
+    //Charts
+    var numberRecordsND = dc.numberDisplay("#number-records-nd");
+	var timeChart = dc.barChart("#time-chart");
+	var economiaChart = dc.rowChart("#economia-row-chart");
+	var tipoChart = dc.rowChart("#tipo-row-chart");
+	var locationChart = dc.rowChart("#location-row-chart");
+	var atencionChart = dc.rowChart("#atencion-row-chart");
+    var dataTable = dc.dataTable("#mytable");
+
+
+
+	numberRecordsND
+		.formatNumber(d3.format("d"))
+		.valueAccessor(function(d){return d; })
+		.group(all);
+
+
+	timeChart
+		.width(650)
+		.height(140)
+		.margins({top: 10, right: 50, bottom: 20, left: 20})
+		.dimension(dateDim)
+		.group(numRecordsByDate)
+		.transitionDuration(500)
+		.x(d3.time.scale().domain([minDate, maxDate]))
+		.elasticY(true)
+		.yAxis().ticks(4);
+
+	economiaChart
+        .width(300)
+        .height(100)
+        .dimension(economiaDim)
+        .group(economiaGroup)
+        .ordering(function(d) { return -d.value })
+        .colors(['#6baed6'])
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+    atencionChart
+        .width(300)
+        .height(100)
+        .dimension(atencionDim)
+        .group(atencionGroup)
+        .ordering(function(d) { return -d.value })
+        .colors(['#6baed6'])
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+
+
+	tipoChart
+		.width(300)
+		.height(310)
+        .dimension(tipoDim)
+        .group(tipoGroup)
+        .ordering(function(d) { return -d.value })
+        .colors(['#6baed6'])
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+
+    dataTable
+        .width(768)
+        .height(480)
+        .dimension(dateDim)
+        .group(function(d) {return "";})
+        .columns([function(d) { return d.timestamp.getDate() + "/" + (d.timestamp.getMonth() + 1) + "/" + d.timestamp.getFullYear(); },
+                  function (d) { return d.tipo },
+                  function (d) { return d.municipio},
+                  function (d) { return d.mensaje },
+                  function (d) { return d.atencion }])
+        .sortBy(function (d) { return +d.timestamp })
+        .order(d3.descending);
+
+    locationChart
+    	.width(200)
+		.height(510)
+        .dimension(locationdDim)
+        .group(locationGroup)
+        .ordering(function(d) { return -d.value })
+        .colors(['#6baed6'])
+        .elasticX(true)
+        .labelOffsetY(10)
+        .xAxis().ticks(4);
+
+    var map = L.map('map', {     fullscreenControl: true, fullscreenControl: {
+        pseudoFullscreen: false // if true, fullscreen to page width and height
+    }});
+
+
+	var drawMap = function(){
+
+	    map.setView([24.134744, -102.937580], 4);
+		mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+		L.tileLayer(
+			'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; ' + mapLink + ' Contributors',
+				maxZoom: 15,
+			}).addTo(map);
+
+		//HeatMap
+		var geoData = [];
+		_.each(allDim.top(Infinity), function (d) {
+			geoData.push([d["latitude"], d["longitude"], 1]);
+	      });
+		var heat = L.heatLayer(geoData,{
+			radius: 10,
+			blur: 20, 
+			maxZoom: 1,
+		}).addTo(map);
+
+
+	};
+
+
+	//Draw Map
+	drawMap();
+
+
+
+	//Update the heatmap if any dc chart get filtered
+	dcCharts = [timeChart, economiaChart, tipoChart, locationChart];
+
+	_.each(dcCharts, function (dcChart) {
+		dcChart.on("filtered", function (chart, filter) {
+			map.eachLayer(function (layer) {
+				map.removeLayer(layer)
+			}); 
+			drawMap();
+		});
+	});
+
+	dc.renderAll();
+
+};
+
+map.addControl(new L.Control.Fullscreen({
+    title: {
+        'false': 'View Fullscreen',
+        'true': 'Exit Fullscreen'
+    }
+}));
