a8156a294f133b5245f2ad80feae8f37279dff71
mdurazob <mdurazob@gmail.com>
2020-04-29 12:21:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import render_template
 import json
 
 
+data_path = './input/'
+
+
 def get_location(longitude, latitude, provinces_json):
     
     point = Point(longitude, latitude)
@@ -29,7 +32,7 @@ def index():
 @app.route("/data")
 def get_data():
    ## gen_age_tr = pd.read_csv(data_path + 'gender_age_train.csv')
-    df = pd.read_csv(data_path + '/events.csv')
+    df = pd.read_csv(data_path + 'events.csv')
    ## ph_br_dev_model = pd.read_csv(data_path + 'phone_brand_device_model.csv')
 
    ## df = gen_age_tr.merge(ev, how='left', on='device_id')
